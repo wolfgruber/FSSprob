c4afd0149c44cfc697dc186a414c3d340d827e85
Ludwig Wolfgruber <ludwig@met56.img>
2023-06-13 12:21:13 +0200

    commenting fortran code and generalising it

--- mod_fss.f90
+++ mod_fss.f90
@@ -5,10 +5,13 @@ IMPLICIT NONE
 CONTAINS
 
 SUBROUTINE integral_table (n1, n2, array, table)
+    ! computes the integral lookup table, as the cumulative sum along two
+    ! dimensions. Behaviour like array.cumsum(axis=0).cumsum(axis=1) in numpy
+    ! python
     INTEGER, INTENT(IN) :: n1, n2
-    INTEGER :: i, j
-    REAL, INTENT(IN) :: array(n1, n2)
-    REAL, INTENT(OUT) :: table(n1, n2)
+    INTEGER             :: i, j
+    REAL, INTENT(IN)    :: array(n1, n2)
+    REAL, INTENT(OUT)   :: table(n1, n2)
 
     table(1,:) = array(1,:)
 
@@ -24,14 +27,17 @@ END SUBROUTINE integral_table
 
 
 SUBROUTINE integral_filter (n1, n2, kernel, field_in, field_out)
+    ! look up process in table (field_in). field_in has to be processed by 
+    ! integral_table. Behaviour like scipy.signal.convolve2d(array, kernel,
+    ! mode='same') in python scipy
     INTEGER, INTENT(IN) :: n1, n2, kernel
-    INTEGER :: r0(n1), r1(n1), c0(n2), c1(n2), radius, i, j
-    REAL, INTENT(IN) :: field_in(n1, n2)
-    REAL, INTENT(OUT) :: field_out(n1, n2)
+    INTEGER             :: r0(n1), r1(n1), c0(n2), c1(n2), radius, i, j
+    REAL, INTENT(IN)    :: field_in(n1, n2)
+    REAL, INTENT(OUT)   :: field_out(n1, n2)
     
     radius = FLOOR(kernel/2.)
 
-    IF (MOD(kernel, 2) == 0) THEN
+    IF (MOD(kernel, 2) == 0) THEN ! case even sized kernel
         DO i=1, n1
             r0(i) = MAX(1, i-radius)
             r1(i) = MIN(n1, i+radius)
@@ -42,7 +48,7 @@ SUBROUTINE integral_filter (n1, n2, kernel, field_in, field_out)
             c1(j) = MIN(n2, j+radius)
         END DO
     
-    ELSE
+    ELSE ! case odd sized kernel
         DO i=1, n1
             r0(i) = MAX(1, i-radius-1)
             r1(i) = MIN(n1, i+radius)
@@ -59,14 +65,42 @@ SUBROUTINE integral_filter (n1, n2, kernel, field_in, field_out)
 END SUBROUTINE integral_filter
 
 
-SUBROUTINE compute_fss (n1, n2, kernel, field1, field2, fss)
+SUBROUTINE compute_fss_table (n1, n2, kernel, field1, field2, fss)
+    ! compute fss for precomputed tables. field1 and field2 have to be
+    ! preprocessed by integral_table
     INTEGER, INTENT(IN) :: n1, n2, kernel
-    REAL, INTENT(IN) :: field1(n1, n2), field2(n1, n2)
-    REAL, INTENT(OUT) :: fss
-    REAL :: numinator, denominator, fhat1(n1, n2), fhat2(n1, n2)
+    REAL, INTENT(IN)    :: field1(n1, n2), field2(n1, n2)
+    REAL, INTENT(OUT)   :: fss
+    REAL                :: numinator, denominator
+    REAL                :: fhat1(n1, n2), fhat2(n1, n2)
     
     CALL integral_filter(n1, n2, kernel, field1, fhat1)
     CALL integral_filter(n1, n2, kernel, field2, fhat2)
+    
+    numinator = SUM( (fhat1 - fhat2)**2 )
+    denominator = SUM( fhat1**2 + fhat2**2 )
+    
+    fss = 1. - numinator / denominator
+END SUBROUTINE compute_fss_table
+
+
+SUBROUTINE compute_fss (n1, n2, kernel, field1, field2, fss)
+    ! compute the FSS for one kernel size. The variables field1 and field2 have
+    ! to contain probability values, either binary for deterministic case or in
+    ! [0,1] for probabilisitc case. If different kernel sizes are computed
+    ! fss_one_thrsh is faster, as it reuses the integral tables.
+    INTEGER, INTENT(IN) :: n1, n2, kernel
+    REAL, INTENT(IN)    :: field1(n1, n2), field2(n1, n2)
+    REAL, INTENT(OUT)   :: fss
+    REAL                :: numinator, denominator
+    REAL                :: table1(n1, n2), table2(n1, n2)
+    REAL                :: fhat1(n1, n2), fhat2(n1, n2)
+    
+    CALL integral_table (n1, n2, field1, table1)
+    CALL integral_table (n1, n2, field2, table2)
+      
+    CALL integral_filter(n1, n2, kernel, table1, fhat1)
+    CALL integral_filter(n1, n2, kernel, table2, fhat2)
     
     numinator = SUM( (fhat1 - fhat2)**2 )
     denominator = SUM( fhat1**2 + fhat2**2 )
@@ -76,12 +110,16 @@ END SUBROUTINE compute_fss
 
 
 SUBROUTINE fss_one_thrsh (n1, n2, nkernel, kernel, field1, field2, fss)
+    ! compute the FSS for nkernel kernel sizes. The variables field1 and field2
+    ! have to contain probability values, either binary for deterministic case 
+    ! or in [0,1] for probabilisitc case.
     INTEGER, INTENT(IN) :: n1, n2, nkernel
-    INTEGER, INTENT(IN):: kernel(nkernel)
+    INTEGER, INTENT(IN) :: kernel(nkernel)
     INTEGER :: i
-    REAL, INTENT(IN) :: field1(n1, n2), field2(n1, n2)
-    REAL, INTENT(OUT) :: fss(nkernel)
-    REAL :: numinator, denominator, table1(n1, n2), table2(n1, n2)
+    REAL, INTENT(IN)    :: field1(n1, n2), field2(n1, n2)
+    REAL, INTENT(OUT)   :: fss(nkernel)
+    REAL                :: numinator, denominator
+    REAL                :: table1(n1, n2), table2(n1, n2)
     
     CALL integral_table (n1, n2, field1, table1)
     CALL integral_table (n1, n2, field2, table2)
@@ -93,13 +131,52 @@ SUBROUTINE fss_one_thrsh (n1, n2, nkernel, kernel, field1, field2, fss)
 
             fss(i) = 1. - numinator / denominator
         ELSE
-            CALL compute_fss (n1, n2, kernel(i), table1, table2, fss(i))
+            CALL compute_fss_table(n1, n2, kernel(i), table1, table2, fss(i))
         END IF
     END DO 
 END SUBROUTINE fss_one_thrsh
 
 
+SUBROUTINE fss_prob (n1, n2, n_ens, n_thrsh, n_kernel, fcst, obs, thrsh, kernel, fss)
+    ! compute the FSSprob for one (ensemble forecast, observation) pair and
+    ! different thresholds and kernel sizes. The field forecast has dimensions
+    ! (ensemble size, x, y) and contains real forcast variables.
+    INTEGER, INTENT(IN) :: n1, n2, n_ens, n_thrsh, n_kernel
+    INTEGER, INTENT(IN) :: kernel(n_kernel)
+    INTEGER :: i, j, k, l
+    REAL, INTENT(IN) :: fcst(n_ens, n1, n2), obs(n1, n2), thrsh(n_thrsh)
+    REAL :: fcst_field(n1, n2), obs_field(n1, n2), ens_field(n1, n2)
+    REAL, INTENT(OUT) :: fss(n_thrsh, n_kernel)
+    
+    fss  = 0.
+
+    DO i=1, n_thrsh
+        WHERE (obs >= thrsh(i))
+            obs_field = 1.
+        ELSEWHERE
+            obs_field = 0.
+        END WHERE
+                
+        fcst_field = 0.
+        DO j=1, n_ens
+            WHERE (fcst(j,:,:) >= thrsh(i))
+                ens_field = 1.
+            ELSEWHERE
+                ens_field = 0.
+            END WHERE
+            fcst_field = fcst_field + ens_field
+        END DO
+        fcst_field = fcst_field / n_ens
+                
+        CALL fss_one_thrsh (n1, n2, n_kernel, kernel, fcst_field, obs_field, fss(i,:))
+    END DO
+   
+END SUBROUTINE ensemble_fss_one_lead_time
+
+
 SUBROUTINE ensemble_fss_one_lead_time (n1, n2, n_ens, n_ens_size, n_thrsh, n_kernel, fcst, obs, ens_size, thrsh, kernel, fss)
+    ! compute the FSSprob for one (ensemble forecast, observation) pair and
+    ! different thresholds and kernel sizes. 
     INTEGER, INTENT(IN) :: n1, n2, n_ens, n_ens_size, n_thrsh, n_kernel
     INTEGER, INTENT(IN) :: ens_size(n_ens_size), kernel(n_kernel)
     INTEGER :: i, j, k, l, sample(n_ens_size), ens_idx
