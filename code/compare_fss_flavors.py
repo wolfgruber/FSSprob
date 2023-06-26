a16977d4223150997067b46800a4bebcf9363a77
wolfgruber <ludwig.wolfgruber@gmx.at>
2023-06-26 14:40:50 +0200

    adding FSSsingle

--- code/compare_fss_flavors.py
+++ code/compare_fss_flavors.py
@@ -111,3 +111,49 @@ def fss_mean(fcst, obs, thrsh, window):
                 )
 
     return fss
+
+
+def fss_single(fcst, obs, thrsh, window):
+    '''
+    Compute the mean of the FSS of each forecast ensemble member.
+
+    Parameters
+    ----------
+    fcst : xarray
+        forecast to verify. In each forecast the ensemble members are arranged
+        on the first axis.
+    obs : xarray
+        observation to verify the forecast.
+    thrsh : np.array
+        thresholds for which the FSS is computed.
+    window : np.array
+        windows (neighbourhoods, kernel sizes) for which the FSS is computed.
+
+    Returns
+    -------
+    fss : np.array
+        FSS aranged like [threshold, window].
+    '''
+    n_thrsh = len(thrsh)
+    n_window = len(window)
+    n_ens = fcst.shape[0]
+    
+    fss_mem = np.empty((n_ens, n_thrsh, n_window))
+                
+    for l in range(n_thrsh):
+        thr_fcst = fcst >= thrsh[l]
+        thr_obs = obs >= thrsh[l]
+
+        obs_cache = cf.compute_integral_table(thr_obs)
+
+        for i in range(n_ens):
+            fcst_cache = cf.compute_integral_table(thr_fcst[i,:,:])
+
+            for m in range(n_window):
+                _, _, fss_mem[i,l,m] = cf.compute_fss(
+                    fcst=thr_fcst[i,:,:], obs=thr_obs, window=window[m],
+                    fcst_cache=fcst_cache, obs_cache=obs_cache
+                    )
+    fss = np.mean(fss_mem, axis=0)
+    
+    return fss
